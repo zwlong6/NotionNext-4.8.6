89e28e6273928f921053f19db9247b97ea714f17
zwl <zwlong6@qq.com>
2025-08-23 16:10:42 +0800

    还原icon

--- blog.config.js
+++ blog.config.js
@@ -61,7 +61,7 @@ const BLOG = {
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hi，我是一个打工人,以归零的心态，敢于未知,欢迎来到我的博客🎉',
+    '以归零的心态，敢于未知,Hi，欢迎来到我的博客🎉',
 
   // uuid重定向至 slug
   UUID_REDIRECT: process.env.UUID_REDIRECT || false
